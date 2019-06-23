7539501ebb1ef56f9c62d0f7a2f55165f003f23e
sdim-ua <ingersol@yandex.ua>
2019-06-23 21:05:26 +0400

    Create Solution.java

---
+++ Solution.java
@@ -0,0 +1,21 @@
+/* 
+Параметризованый интерфейс
+*/
+
+public class Solution {
+    public static void main(String[] args) throws Exception {
+    }
+
+    interface SimpleObject<T> {
+        SimpleObject<T> getInstance();
+    }
+
+    class StringObject  implements SimpleObject<String>
+    //допишите здесь ваш код
+    {
+         @Override
+        public SimpleObject<String> getInstance(){
+            return null;
+        }
+    }
+}
